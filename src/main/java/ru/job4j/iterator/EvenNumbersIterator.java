07e81b56e78936388807646e544cbf70dacfc268
Oklevet <nikstar1235@gmail.com>
2021-02-09 13:05:53 +0300

    Copy from job4j_elementary

---
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -0,0 +1,38 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumbersIterator implements Iterator<Integer> {
+    private int[] ints;
+    private int position = -1;
+    private int nextPosition = -1;
+
+    public EvenNumbersIterator(int[] ints) {
+        this.ints = ints;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position != findEven();
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        position = nextPosition;
+        return ints[position];
+    }
+
+    public Integer findEven() {
+        for (int i = position + 1; i < ints.length; i++) {
+            if (ints[i] % 2 == 0) {
+                nextPosition = i;
+                break;
+            }
+        }
+       return nextPosition;
+    }
+}
---
+++ src/test/java/ru/job4j/iterator/BackwardArrayItTest.java
@@ -0,0 +1,7 @@
+package ru.job4j.iterator;
+
+import junit.framework.TestCase;
+
+public class BackwardArrayItTest extends TestCase {
+
+}
