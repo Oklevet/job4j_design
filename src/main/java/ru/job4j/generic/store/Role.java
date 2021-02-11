b682789249f4b7174d552d8eaf633724f4981180
Oklevet <nikstar1235@gmail.com>
2021-02-11 10:54:30 +0300

    5.2.2. Реализовать Store<T extends Base> [#466926]

--- src/main/java/ru/job4j/generic/store/Role.java
+++ src/main/java/ru/job4j/generic/store/Role.java
@@ -4,9 +4,4 @@ public class Role extends Base {
     public Role(String id) {
         super(id);
     }
-
-    @Override
-    public String getId() {
-        return super.getId();
-    }
 }
--- src/main/java/ru/job4j/generic/store/User.java
+++ src/main/java/ru/job4j/generic/store/User.java
@@ -6,9 +6,4 @@ public class User extends Base {
     public User(String id) {
         super(id);
     }
-
-    @Override
-    public String getId() {
-        return super.getId();
-    }
 }
