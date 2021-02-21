ed1fcdeecc45ffa4ef017bf444cb2cc9c2ba4050
Oklevet <nikstar1235@gmail.com>
2021-02-22 00:12:25 +0300

    0.2. FileInputStream [#466962]

---
+++ src/main/java/ru/job4j/collection/io/EvenNumberFile.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection.io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder num = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                if (read == 13) {
+                    System.out.println(num + " is "
+                            + (Integer.parseInt(String.valueOf(num)) % 2 == 0 ? " EVEN" : "ODD"));
+                } else if (read == 10) {
+                    num.setLength(0);
+                } else {
+                    num.append((char) read);
+                }
+            }
+            System.out.println(num + " is "
+                            + (Integer.parseInt(String.valueOf(num)) % 2 == 0 ? " EVEN" : "ODD"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
