3e013b08d53b328c89ff10a4e30f444204a0f50c
Oklevet <nikstar1235@gmail.com>
2021-02-26 16:29:45 +0300

    3.0. Тестирование IO [#466950]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,88 +1,98 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+    Analizy analizy = new Analizy();
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void addFewWorking() {
-        Analizy analizy = new Analizy();
-        File actualFile = new File("serverLogsTests/serverLog1.csv");
-        analizy.unavailable("serverLogsTests/serverLog1.txt", "serverLogsTests/serverLog1.csv");
-        List<String> list = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(actualFile))) {
-            list = reader
-                    .lines()
-                    .collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void addFewWorking() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
         }
-        List<String> listExpect = new ArrayList<>();
-        listExpect.add("10:57:01;10:59:01");
-        listExpect.add("11:01:02;11:02:02");
-        assertThat(list, is(listExpect));
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            reader.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01;10:59:0111:01:02;11:02:02"));
     }
 
     @Test
-    public void allNotWork() {
-        Analizy analizy = new Analizy();
-        File actualFile = new File("serverLogsTests/serverLog2.csv");
-        analizy.unavailable("serverLogsTests/serverLog2.txt", "serverLogsTests/serverLog2.csv");
-        List<String> list = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(actualFile))) {
-            list = reader
-                    .lines()
-                    .collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void allNotWork() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("400 10:59:01");
+            out.println("500 11:01:02");
+            out.println("500 11:02:02");
         }
-        List<String> listExpect = new ArrayList<>();
-        listExpect.add("10:57:01; still not working");
-        assertThat(list, is(listExpect));
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            reader.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01; still not working"));
     }
 
     @Test
-    public void allWork() {
-        Analizy analizy = new Analizy();
-        File actualFile = new File("serverLogsTests/serverLog3.csv");
-        analizy.unavailable("serverLogsTests/serverLog3.txt", "serverLogsTests/serverLog3.csv");
-        List<String> list = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(actualFile))) {
-            list = reader
-                    .lines()
-                    .collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void allWork() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:57:01");
+            out.println("100 10:58:01");
+            out.println("300 10:59:01");
+            out.println("700 11:01:02");
+            out.println("900 11:02:02");
         }
-        List<String> listExpect = new ArrayList<>();
-        assertThat(list, is(listExpect));
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            reader.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is(""));
     }
 
     @Test
-    public void firstNotWork() {
-        Analizy analizy = new Analizy();
-        File actualFile = new File("serverLogsTests/serverLog4.csv");
-        analizy.unavailable("serverLogsTests/serverLog4.txt", "serverLogsTests/serverLog4.csv");
-        List<String> list = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(actualFile))) {
-            list = reader
-                    .lines()
-                    .collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void firstNotWork() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("400 10:58:00");
+            out.println("300 10:58:01");
+            out.println("200 10:59:01");
+            out.println("100 11:01:02");
+            out.println("100 11:02:02");
         }
-        List<String> listExpect = new ArrayList<>();
-        listExpect.add("10:58:00;10:58:01");
-        assertThat(list, is(listExpect));
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            reader.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:58:00;10:58:01"));
     }
 }
