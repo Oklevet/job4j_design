5a7366fac30107bea4fa2d1137cf6d636cf7dfa9
Oklevet <nikstar1235@gmail.com>
2021-04-15 21:09:54 +0300

    Хранилище машин [#466889]

---
+++ src/SQL/car reverse.sql
@@ -0,0 +1,66 @@
+drop table cars;
+drop table body;
+drop table engine;
+drop table transmission;
+
+create table body(
+	id serial primary key,
+	elements varchar
+);
+
+create table engine(
+	id serial primary key,
+	elements varchar
+);
+
+create table transmission(
+	id serial primary key,
+	elements varchar
+);
+
+create table cars(
+	id serial primary key,
+	model varchar,
+	elem_b_id int references body(id),
+	elem_e_id int references engine(id),
+	elem_t_id int references transmission(id)
+);
+
+insert into body(elements) values ('door');
+insert into body(elements) values ('hood');
+insert into body(elements) values ('window');
+insert into body(elements) values ('tail');
+insert into body(elements) values ('spoiler');
+insert into engine(elements) values ('nozzles');
+insert into engine(elements) values ('pulley');
+insert into engine(elements) values ('pusher');
+insert into engine(elements) values ('cylinder liner');
+insert into transmission(elements) values ('pump');
+insert into transmission(elements) values ('planetary reductor');
+insert into transmission(elements) values ('oil receiver');
+insert into transmission(elements) values ('clutch');
+insert into transmission(elements) values ('torque converter');
+
+
+insert into cars(model, elem_b_id, elem_e_id, elem_t_id) values ('Ferrari', 1, 2, 1);
+insert into cars(model, elem_b_id, elem_e_id, elem_t_id) values ('Ferrari', 2, 3, 2);
+insert into cars(model, elem_b_id, elem_e_id, elem_t_id) values ('Ferrari', 3, 4, 3);
+insert into cars(model, elem_b_id, elem_e_id, elem_t_id) values ('Ferrari', 5, null, 4);
+insert into cars(model, elem_b_id, elem_e_id, elem_t_id) values ('Ferrari', null, null, 5);
+insert into cars(model, elem_b_id, elem_e_id, elem_t_id) values ('Lada', 1, 2, 1);
+insert into cars(model, elem_b_id, elem_e_id, elem_t_id) values ('Lada', 2, 3, 2);
+insert into cars(model, elem_b_id, elem_e_id, elem_t_id) values ('Lada', 3, 4, 3);
+insert into cars(model, elem_b_id, elem_e_id, elem_t_id) values ('Lada', 5, null, 4);
+
+select * from body
+select * from engine
+select * from transmission
+select * from cars
+
+select c.model, b.elements, e.elements, t.elements
+    from cars c left join body b on c.elem_b_id = b.id
+                left join engine e on c.elem_e_id = e.id
+                left join transmission t on c.elem_t_id = t.id
+
+select * from body b where b.id = 4;
+select * from engine e where e.id = 1; 
