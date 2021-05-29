1ec2217f949a81699faf81c42b6d1580c541d720
Oklevet <nikstar1235@gmail.com>
2021-05-29 22:01:30 +0300

    1. Хранилище продуктов [#852]

--- src/main/java/ru/job4j/odd/lsp/foodstorage/ControllQuality.java
+++ src/main/java/ru/job4j/odd/lsp/foodstorage/ControllQuality.java
@@ -5,20 +5,50 @@ import java.time.Period;
 
 public class ControllQuality {
     private short discount;
+    private final double quat     = 0.25D;
+    private final double toQuat   = 0.75D;
+    private final double full     = 1D;
 
     void locateFood(Food food) {
-        double coeffDelay = Double.parseDouble(String.valueOf(Period.between(LocalDate.now(), food.createDate).getDays()))
-                / Double.parseDouble(String.valueOf(Period.between(food.createDate, food.expiryDate).getDays()));
+        double presentExpir = Double.parseDouble(String.valueOf(Period.between(LocalDate.now(),
+                food.createDate).getDays()));
+        double fullExpir    = Double.parseDouble(String.valueOf(Period.between(food.createDate,
+                food.expiryDate).getDays()));
+        double coeffDelay = presentExpir / fullExpir;
+        double yesterCoeff = (presentExpir - 1) / fullExpir;
 
-        if (coeffDelay < 0.25) {
-            new Warehouse().addFood(food);
-        } else if (coeffDelay >= 0.25 && coeffDelay < 0.75) {
-            new Shop().addFood(food);
-        } else if (coeffDelay >= 0.75 && coeffDelay <= 1) {
-            food.setDiscount(discount);
-            new Shop().addFood(food);
+        if (coeffDelay < quat) {
+            Warehouse wh = new Warehouse();
+            if (!wh.warehouse.contains(food)) {
+                wh.addFood(food);
+            }
+        } else if (coeffDelay < toQuat) {
+            Shop sh = new Shop();
+            if (yesterCoeff < toQuat) {
+                if (!sh.shop.contains(food)) {
+                    sh.addFood(food);
+                }
+            } else {
+                sh.addFood(food);
+                new Warehouse().warehouse.remove(food);
+            }
+        } else if (coeffDelay <= full) {
+            Shop sh = new Shop();
+            if (yesterCoeff < full) {
+                if (!sh.shop.contains(food)) {
+                    food.setDiscount(discount);
+                    sh.addFood(food);
+                } else if (yesterCoeff < toQuat) {
+                    sh.shop.remove(food);
+                    food.setDiscount(discount);
+                    sh.addFood(food);
+                }
+            }
         } else {
             new Trash().addFood(food);
+            if (yesterCoeff < full) {
+                new Shop().shop.remove(food);
+            }
         }
     }
 }
