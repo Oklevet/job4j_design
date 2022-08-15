15b270a2bd9a2b8d0865cd1dae22221d93713943
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-08-15 20:40:15 +0500

    Task 16. Replenishment. Correct style.

--- src/main/java/ru/job4j/tasks/Task16.java
+++ src/main/java/ru/job4j/tasks/Task16.java
@@ -8,8 +8,8 @@ public class Task16 {
         String strPlus = source.substring(0, 1);
         String strExclam = source.substring(1, 2);
         String strQuast = source.substring(2, 3);
-        String res = new StringJoiner("").add(strPlus).add(strPlus).add(strPlus).add(strExclam).add(strExclam).add(strExclam)
-                .add(strExclam).add(strQuast).add(strQuast).toString();
+        String res = new StringJoiner("").add(strPlus).add(strPlus).add(strPlus).add(strExclam).add(strExclam)
+                .add(strExclam).add(strExclam).add(strQuast).add(strQuast).toString();
         System.out.print(res);
     }
 }
