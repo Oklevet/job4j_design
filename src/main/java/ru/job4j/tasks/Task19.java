17a31e999dfe1b63ee6da703071329a67794e33f
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-08-15 10:23:13 +0500

    Task 19. Replenishment.

---
+++ src/main/java/ru/job4j/tasks/Task19.java
@@ -0,0 +1,13 @@
+package ru.job4j.tasks;
+
+public class Task19 {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 11;
+        int sum = x + y;
+        int multiple = x * y;
+        System.out.println("Значение x равно " + x + "; значение y равно " + y);
+        System.out.println("Произведение x и y равно " + multiple);
+        System.out.print("Сумма x и y равно " + sum);
+    }
+}
