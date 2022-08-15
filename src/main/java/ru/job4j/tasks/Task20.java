0a1afe8e315c1638c00d59a9235ba9bf60cedb8b
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-08-15 10:09:33 +0500

    Task 20. Replenishment.

---
+++ src/main/java/ru/job4j/tasks/Task20.java
@@ -0,0 +1,13 @@
+package ru.job4j.tasks;
+
+public class Task20 {
+    public static void main(String[] args) {
+        String fullName = "Фамилия Имя Отчество";
+        String phoneNumber = "+7(938) 123-45-67";
+        String stars = "************************";
+        System.out.println(stars);
+        System.out.println("* " + fullName + " *");
+        System.out.println("* " + phoneNumber + "    *");
+        System.out.print(stars);
+    }
+}
