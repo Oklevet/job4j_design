93667eb0a09cc4bd3bb1a5c6dae18b01ef47fad7
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-08-15 20:33:39 +0500

    Task 16. Replenishment.

---
+++ src/main/java/ru/job4j/tasks/Task16.java
@@ -0,0 +1,15 @@
+package ru.job4j.tasks;
+
+import java.util.StringJoiner;
+
+public class Task16 {
+    public static void main(String[] args) {
+        String source = "+!?";
+        String strPlus = source.substring(0, 1);
+        String strExclam = source.substring(1, 2);
+        String strQuast = source.substring(2, 3);
+        String res = new StringJoiner("").add(strPlus).add(strPlus).add(strPlus).add(strExclam).add(strExclam).add(strExclam)
+                .add(strExclam).add(strQuast).add(strQuast).toString();
+        System.out.print(res);
+    }
+}
---
+++ src/test/java/ru/job4j/tasks/Task16Test.java
@@ -0,0 +1,20 @@
+package ru.job4j.tasks;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class Task16Test {
+    @Test
+    public void eachSequenceOfSameSymbolsOnSeparateLine() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task16.main(null);
+        assertThat(out.toString(), is("+++!!!!??"));
+    }
+
+}
