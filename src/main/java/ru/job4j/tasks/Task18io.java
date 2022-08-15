5e4fa33fb237745a2495d331d9db140a59e2745e
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-08-15 11:04:05 +0500

    Task 18.1. Replenishment.

---
+++ src/main/java/ru/job4j/tasks/Task18io.java
@@ -0,0 +1,9 @@
+package ru.job4j.tasks;
+
+public class Task18io {
+    public static void main(String[] args) {
+        int x = 9;
+        System.out.println("Значение x равно " + x);
+        System.out.print("Значение x^2 равно " + x * x);
+    }
+}
---
+++ src/test/java/ru/job4j/tasks/Task18ioTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.tasks;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
+public class Task18ioTest {
+    @Test
+    public void shouldOutputStringPlusNumber() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task18io.main(null);
+        String expected = "Значение x равно 9" + System.lineSeparator() + "Значение x^2 равно 81";
+        assertThat(out.toString(), is(expected));
+    }
+}
