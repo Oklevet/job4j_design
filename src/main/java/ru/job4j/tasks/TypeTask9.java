f922fb9a3446ee251b6dfbce2a451f20fad0c89a
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-08-15 21:08:01 +0500

    Types. Task 9. Replenishment.

---
+++ src/main/java/ru/job4j/tasks/TypeTask9.java
@@ -0,0 +1,15 @@
+package ru.job4j.tasks;
+
+import java.util.Scanner;
+
+public class TypeTask9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+    }
+}
