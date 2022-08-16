67f61db18770256b065c1885eade4332a6f67850
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-08-16 16:24:57 +0500

    Arrays. Task6_5. Replenishment.

---
+++ src/main/java/ru/job4j/tasks/ArraysTask6_5.java
@@ -0,0 +1,25 @@
+package ru.job4j.tasks;
+
+import java.util.ArrayList;
+
+public class ArraysTask6_5 {
+    public static char[] onlyLowCase(char[] input) {
+        ArrayList<Character> arrayList = new ArrayList<>();
+        int i = 0;
+
+        for(Character ch : input) {
+            if (Character.isLetter(ch) && Character.isLowerCase(ch)) {
+                arrayList.add(Character.toUpperCase(ch));
+            }
+        }
+
+        char[] resChar = new char[arrayList.size()];
+
+        for (Character character : arrayList) {
+            resChar[i] = character;
+            i++;
+        }
+
+        return resChar;
+    }
+}
