380cd02c18a78afce95806dafe8cc800fd4c6bfe
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-11-01 23:45:40 +0500

    typeTask25

---
+++ src/main/java/ru/job4j/tasks/type/Task25.java
@@ -0,0 +1,11 @@
+package ru.job4j.tasks.type;
+
+public class Task25 {
+    public static void separate(int number) {
+        int num1, num2, num3;
+        num1 = number / 100 * 100;
+        num2 = number / 10 % 10 * 10;
+        num3 = number % 10;
+        System.out.println(num1 + "+" + num2 + "+" + num3);
+    }
+}
