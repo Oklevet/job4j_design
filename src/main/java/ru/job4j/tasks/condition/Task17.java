d1b6f6fd73ff55d5445543ccf844017cc0ca665b
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-11-02 21:45:47 +0500

    conditionTask17

---
+++ src/main/java/ru/job4j/tasks/condition/Task17.java
@@ -0,0 +1,15 @@
+package ru.job4j.tasks.condition;
+
+public class Task17 {
+    public static void signCheck(int number) {
+        if (number < 0) {
+            System.out.println("Отрицательное");
+        }
+        else if (number == 0) {
+            System.out.println("Ноль");
+        }
+        else {
+            System.out.println("Положительное");
+        }
+    }
+}
