e7d0fd1264acf8e32820b074071294bad6aca994
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-11-02 09:30:39 +0500

    typeTask38

---
+++ src/main/java/ru/job4j/tasks/type/Task38.java
@@ -0,0 +1,9 @@
+package ru.job4j.tasks.type;
+
+public class Task38 {
+    public static void leftShift(int number) {
+        int num1, num2;
+        num1 = number % 1000 * 10;
+        System.out.println(num1);
+    }
+}
