2d78dcee465f99df36b0d09c65266bfe0b4fa037
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-11-02 21:57:34 +0500

    conditionTask18

---
+++ src/main/java/ru/job4j/tasks/condition/Task18.java
@@ -0,0 +1,7 @@
+package ru.job4j.tasks.condition;
+
+public class Task18 {
+    public static void numDecrease(int number) {
+        System.out.println(number >= 100 ? --number : number);
+    }
+}
