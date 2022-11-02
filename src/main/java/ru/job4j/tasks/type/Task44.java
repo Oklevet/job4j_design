afb8160cda440c0bf853cf95e1d5c5aba2e44f7b
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-11-02 11:35:30 +0500

    typeTask44

---
+++ src/main/java/ru/job4j/tasks/type/Task44.java
@@ -0,0 +1,11 @@
+package ru.job4j.tasks.type;
+
+public class Task44 {
+    public static void rightNumChange(int number1, int number2) {
+        int num1, num2;
+        num1 = number1 % 10;
+        num2 = number2 % 10;
+        System.out.println(number1 - num1 + num2);
+        System.out.println(number2 - num2 + num1);
+    }
+}
