4e1cfaeacec00a48e064203a1d57ba3d2e240e13
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-11-02 09:02:22 +0500

    typeTask34

---
+++ src/main/java/ru/job4j/tasks/type/Task34.java
@@ -0,0 +1,27 @@
+package ru.job4j.tasks.type;
+
+public class Task34 {
+    public static void evenAmount(int number) {
+        int num1, num2, num3, num4, counter = 0;
+        num1 = number / 1000;
+        num2 = number / 100 % 10;
+        num3 = number / 10 % 10;
+        num4 = number % 10;
+        if (num1 % 2 == 0) {
+            counter++;
+        }
+
+        if (num2 % 2 == 0) {
+            counter++;
+        }
+
+        if (num3 % 2 == 0) {
+            counter++;
+        }
+
+        if (num4 % 2 == 0) {
+            counter++;
+        }
+        System.out.println(counter);
+    }
+}
