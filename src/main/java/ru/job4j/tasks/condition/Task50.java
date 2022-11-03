4b9e39efba6f7c052801954aabcbe0fe3caed686
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-11-03 22:05:47 +0500

    conditionTask50

---
+++ src/main/java/ru/job4j/tasks/condition/Task50.java
@@ -0,0 +1,12 @@
+package ru.job4j.tasks.condition;
+
+public class Task50 {
+    public static void maxLastDigit(int num1, int num2) {
+        int fnum1, fnum2;
+        fnum1 = num1 % 10;
+        fnum2 = num2 % 10;
+        if (fnum1 == fnum2) {
+            System.out.println("Одинаковые");
+        } else System.out.println(Math.max(fnum1, fnum2));
+    }
+}
