929d60c6cdd6765fdb35552e1a8d1d2f8b0bd128
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-11-03 10:17:14 +0500

    conditionTask31

---
+++ src/test/java/ru/job4j/tasks/condition/Task31Test.java
@@ -0,0 +1,41 @@
+package ru.job4j.tasks.condition;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class Task31Test {
+    public String ln = System.lineSeparator();
+
+    @Test
+    public void when1And3Then2() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task31.calculate(1, 3);
+        String expected = "2" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+
+    @Test
+    public void when5And2Then3() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task31.calculate(5, 2);
+        String expected = "3" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+
+    @Test
+    public void when3And3Then9() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task31.calculate(3, 3);
+        String expected = "9" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+
+}
