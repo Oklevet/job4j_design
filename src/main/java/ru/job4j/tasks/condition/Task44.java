c3f29bef919de53a6c322349f493b83b037ccf0e
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-11-03 11:02:27 +0500

    conditionTask44

---
+++ src/main/java/ru/job4j/tasks/condition/Task44.java
@@ -0,0 +1,7 @@
+package ru.job4j.tasks.condition;
+
+public class Task44 {
+    public static void containsZero(int number) {
+        System.out.println(Math.round((double) number / 10) * 10);
+    }
+}
