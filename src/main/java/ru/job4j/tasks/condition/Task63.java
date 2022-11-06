6abc911a6026dd0e2893cc7bac705e24da076ea4
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-11-06 11:57:23 +0500

    conditionTask63

---
+++ src/main/java/ru/job4j/tasks/condition/Task63.java
@@ -0,0 +1,15 @@
+package ru.job4j.tasks.condition;
+
+public class Task63 {
+    public static void isDecreasing(int number) {
+        int num1, num2, num3;
+        num1 = number / 100;
+        num2 = number / 10 % 10;
+        num3 = number % 10;
+        if (num1 > num2 && num2 > num3) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+}
