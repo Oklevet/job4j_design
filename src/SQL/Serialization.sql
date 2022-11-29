46c672b49b02b4b7c42e40ced3ec83dae64f809d
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-11-29 21:35:47 +0500

    upd 4 bot

---
+++ src/SQL/Serialization.sql
@@ -0,0 +1,18 @@
+--psql -h 127.0.0.1 -U postgres
+begin transaction isolation level serializable;
+create table sales (
+    id serial primary key,
+    name varchar(50),
+    price integer
+);
+
+insert into sales (name, price) VALUES ('sale1', 10);
+insert into sales (name, price) VALUES ('sale2', 20);
+insert into sales (name, price) VALUES ('sale3', 30);
+
+select sum(price) from sales;
+
+update sales set price = 100 where name = 'sale1';
+update sales set price = 300 where name = 'sale3';
+
+commit;
