aec3f10e8dc27df93df2ed95a58a28eaf2a738cb
Oklevet <75887570+Oklevet@users.noreply.github.com>
2023-02-13 16:24:01 +0500

    9. Генерирование колоды карт. FlatMap [#504873]

---
+++ src/main/java/ru/job4j/lambda/Card.java
@@ -0,0 +1,28 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Card {
+    private final Suit suit;
+    private final Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" + "suit=" + suit
+                + ", value=" + value + '}';
+    }
+
+    public static List<Card> makeCardDeck() {
+        return Arrays.stream(Suit.values()).flatMap(x -> Arrays.stream(Value.values()).map(y -> new Card(x, y))).toList();
+    }
+
+    public static void main(String[] args) {
+        makeCardDeck().forEach(x -> System.out.println(x.toString()));
+    }
+}
