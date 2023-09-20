40edd84468ae161970c9368b1b8044f0e99f1ecd
Oklevet <75887570+Oklevet@users.noreply.github.com>
2023-09-20 13:19:06 +0500

    Task 127 Loop

---
+++ src/main/java/ru/job4j/tasks/loop/Task127.java
@@ -0,0 +1,8 @@
+package ru.job4j.tasks.loop;
+
+public class Task127 {
+    public static void loop(int num) {
+        char[] chars = String.valueOf(num).toCharArray();
+        System.out.println(chars[0]);
+    }
+}
