cfab147f75b34e0574f4bd836d1b214ab9077193
Oklevet <nikstar1235@gmail.com>
2023-10-11 18:40:08 +0300

    2. @Setter, @Getter и другие [#504889 #451929]

--- src/main/java/ru/job4j/mapstuct/Main.java
+++ src/main/java/ru/job4j/mapstuct/Main.java
@@ -20,6 +20,7 @@ public class Main {
 
         System.out.println();
 
+
         DeliveryAddressMapper deliveryAddressMapper = Mappers.getMapper(DeliveryAddressMapper.class);
         sEntity = new StudentEntity(0, "entity", "junior");
         sDto = new StudentDto(11, "dto", "middle");
