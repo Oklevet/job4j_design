3fb5aaaf2d6267d927b43082d25ac4a2fa3aedb5
Oklevet <nikstar1235@gmail.com>
2025-05-31 17:57:13 +0300

    Task70

---
+++ src/main/java/ru/job4j/tasks/oneDimArray/Task70.java
@@ -0,0 +1,18 @@
+package ru.job4j.tasks.oneDimArray;
+
+import java.util.StringJoiner;
+
+public class Task70 {
+    public static void array(int[] arr) {
+        StringJoiner line1 = new StringJoiner(" ");
+        StringJoiner line2 = new StringJoiner(" ");
+        for (int i = 0; i < arr.length; i++) {
+            if (i <= Math.ceil((arr.length - 1) / 2)) {
+                line1.add(String.valueOf(arr[i]));
+            } else {
+                line2.add(String.valueOf(arr[i]));
+            }
+        }
+        System.out.println(line1 + System.lineSeparator() + line2);
+    }
+}
