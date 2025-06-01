38f2dc5a1396dea2d200a14523b1ca2702e59a78
Oklevet <nikstar1235@gmail.com>
2025-06-01 09:15:23 +0300

    Task83

---
+++ src/main/java/ru/job4j/tasks/oneDimArray/Task83.java
@@ -0,0 +1,20 @@
+package ru.job4j.tasks.oneDimArray;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task83 {
+    public static int[] sumOfTwo(int[] numbers, int goal) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(goal - numbers[i])) {
+                res[0] = map.get(goal - numbers[i]);
+                res[1] = i;
+            }
+            map.putIfAbsent(numbers[i], i);
+        }
+        return res[0] == res[1] ? null : res;
+    }
+}
