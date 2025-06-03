b94c6b21e0b1bc4553db467276ae21c06eaa4cde
Oklevet <nikstar1235@gmail.com>
2025-06-03 08:44:03 +0300

    ChangeMachine (not finished)

---
+++ src/main/java/ru/job4j/tasks/biDimArray/ChangeMachine.java
@@ -0,0 +1,79 @@
+package ru.job4j.tasks.biDimArray;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ChangeMachine {
+    static int amountSum;
+    List<int[]> variants = new ArrayList<>();
+
+    public List<int[]> getChangeVariants(int amount) {
+        int[] nums = {10, 5, 2, 1};
+        int[] empty = {};
+
+        if (amount == 0) {
+            return new ArrayList<>();
+        }
+
+        amountSum = amount;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (amountSum % nums[i] == 0) {
+                variants.add(addArrSimple(i, amountSum / nums[i]));
+            }
+            variants.addAll(calcByHead(nums, i, new int[] {0, 0, 0, 0}));
+        }
+
+        return variants;
+    }
+
+    public static List<int[]> calcByHead(int[] nums, int startId, int[] res) {
+        List<int[]> vars = new ArrayList<>();
+        System.out.println("res = " + Arrays.toString(res));
+
+        if (startId > nums.length - 1) {
+            vars.add(res);
+            return vars;
+        }
+
+        int[] result = res;
+        int start = nums[startId];
+        int prevStartId = startId;
+        int currSum = amountSum - Arrays.stream(res).sum();
+
+        while (startId < nums.length){  // || result[result.length - 1] < currSum / nums[3]) {
+            System.out.println("startId = " + startId + ";  nums.length = " + nums.length);
+            prevStartId = startId;
+            for (int j = 1; j < currSum / start; j++) {
+                result[startId] = j;
+
+                startId++;
+                System.out.println(1);
+                vars.addAll(calcByHead(nums, startId, result));
+
+                if (startId >= nums.length - 1) {
+                    break;
+                }
+            }
+            System.out.println("prevStartId = " + prevStartId + ";  nums.length = " + nums.length);
+            if (prevStartId < nums.length - 1) {
+                System.out.println(2);
+                vars.addAll(calcByHead(nums, ++prevStartId, result));
+            }
+            System.out.println("prevStartId = " + prevStartId + ";  startId = " + startId);
+            if (startId == nums.length - 1) {
+                return vars;
+            }
+        }
+        //vars.add(new int[] {n1, n2, n3, n4});
+        return vars;
+    }
+
+    public static int[] addArrSimple(int i, int num) {
+        int[] defArr = {0, 0, 0, 0};
+        defArr[i] = num;
+        return defArr;
+    }
+}
