c08ec9963941a0738ea039687ab518ab05287d02
Oklevet <nikstar1235@gmail.com>
2025-06-13 15:15:53 +0300

    7. Балансир [#505083 #546053]

--- src/main/java/ru/job4j/iterator/Balancer.java
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Balancer {
     public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
         int cnt = 0;
+
         while (source.hasNext()) {
             nodes.get(cnt++).add(source.next());
             if (cnt == nodes.size()) {
